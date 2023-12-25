032266708fb2ecaeb003bf01cabcdbebe2aa9507
romain jacquesson <romain.jacquesson@gmail.com>
2023-12-25 18:26:09 +0100

    finalisation

--- src/pages/Apropos/index.jsx
+++ src/pages/Apropos/index.jsx
@@ -14,7 +14,7 @@ const Apropos = () => {
                     <a className="github_link" href="https://github.com/Roriks22" target="_blank" rel="noopener noreferrer">
                         <i className="fa-brands fa-github" style={{ color: '#000000' }}></i>GitHub
                     </a>
-                    <a className="cv_link" href="https://julienjacquesson.files.wordpress.com/2023/12/cv-romain-jacquesson.pdf" target="_blank" rel='noopener noreferrer'>
+                    <a className="cv_link" href="https://romainjacquessoncv.files.wordpress.com/2023/12/cv-romain-jacquesson-2.pdf" target="_blank" rel='noopener noreferrer'>
                         Afficher mon CV 
                     </a>  
                 </div>                
